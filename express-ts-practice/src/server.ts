77a1138ab0114148cd7ef41f388b10e1d2702105
tejayy <tbj456@gmail.com>
2026-05-28 22:35:17 +0530

    Backend Setup

--- express-ts-practice/src/server.ts
+++ express-ts-practice/src/server.ts
@@ -1,17 +1,20 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('API Running');
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
