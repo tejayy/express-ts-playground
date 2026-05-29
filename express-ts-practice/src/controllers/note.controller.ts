6a73a3e608e1245cb98a28a20e340e7db4d725dd
tejayy <tbj456@gmail.com>
2026-05-29 16:09:36 +0530

    Filters and search added with pagination

--- express-ts-practice/src/controllers/note.controller.ts
+++ express-ts-practice/src/controllers/note.controller.ts
@@ -30,10 +30,33 @@ export const createNote = asyncHandler(
 
 export const getNotes = asyncHandler(
   async (req: CustomerRequest, res: Response) => {
+    const page = Number(req.query.page) || 1;
+    const search = (req.query.search as string) || '';
+    const limit = Number(req.query.limit) || 5;
+    const skip = (page - 1) * limit;
+
+    const completed = req.query.completed === 'true';
+
+    const sortOrder = req.query.sort === 'asc' ? 'asc' : 'desc';
+
     const notes = await prisma.note.findMany({
       where: {
         userId: req.user!.id,
+
+        completed: req.query.completed !== undefined ? completed : undefined,
+
+        title: {
+          contains: search,
+          mode: 'insensitive',
+        },
       },
+
+      orderBy: {
+        createdAt: sortOrder,
+      },
+
+      skip,
+      take: limit,
     });
 
     res.status(200).json({
